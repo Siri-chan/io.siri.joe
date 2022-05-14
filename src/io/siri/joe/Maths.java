d0517b14b175544188f56023fa994cc24eab6e15
Siri <xd42069xd69420@gmail.com>
2022-05-14 18:12:43 +0900

    Added new clamp function for longs

--- src/io/siri/joe/Maths.java
+++ src/io/siri/joe/Maths.java
@@ -4,6 +4,9 @@ public class Maths {
     public static int clamp(int v, int min, int max) {
         return (v > max) ? max : Math.max(v, min);
     }
+    public static long clamp(long v, long min, long max) {
+        return (v > max) ? max : Math.max(v, min);
+    }
     public static float clamp(float v, float min, float max) {
         return (v > max) ? max : Math.max(v, min);
     }
